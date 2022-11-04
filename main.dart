ae8c69a7967dfdcf46fb58c4e46902f116f245cc
Able <ablekilinda@gmail.com>
2022-11-04 03:36:58 +0300

    dart classes

---
+++ main.dart
@@ -0,0 +1,4 @@
+main (){
+    int ableLotan = "hellow";
+    print(ableLotan);
+}
---
+++ zero.dart
@@ -0,0 +1,21 @@
+
+
+
+class Lotan{
+  int length=2;
+  int width=5;
+
+  Lotan (){
+  length =4;
+  width =5;
+}
+
+
+main (){
+int area = length * width ;
+print(area);
+
+}
+
+
+}
