abf72058d90f786c1dd0f4f0bdb6e654086eddbd
Able <ablekilinda@gmail.com>
2022-11-04 13:46:41 +0300

    adding setters

---
+++ getset.dart
@@ -0,0 +1,28 @@
+//getter and setter in dart
+class Student {
+  String? _firstname;
+  String? _lastname;
+  int? _Age;
+
+  //getter to get fullname
+  String get fullname => this._firstname! + " " + this._lastname!;
+  //getter to read private age property
+  int get age => this._Age!;
+
+  //setter to set irstname
+  set firstname(String firstname) => this._firstname = firstname;
+
+  //setter to set lastname
+  set lastname(String lastname) => this._lastname = lastname;
+
+  //setter to set age
+  set age(int age) {
+    if (this._Age < 0) {
+      throw new Exception("Age cant be less than 0");
+    }
+    this._Age = age;
+  }
+}
+
+//main method
+void main() {}
